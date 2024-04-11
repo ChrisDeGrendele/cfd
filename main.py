6e05bcf507e9e136a755b4396128109a779fabf9
Chris DeGrendele <chris@Chriss-MacBook-Pro.local>
2024-04-10 21:43:21 -0700

    Main code layout

---
+++ main.py
@@ -0,0 +1,19 @@
+from src.grid import Grid1D
+import numpy as np
+
+
+nt = 100
+nx = 100
+xlim = (0,1)
+nghost = 2
+numVariables = 1
+ics = np.sin
+
+g = Grid1D(xlim, nx, nghost, numVariables)
+g.fill_grid(ics,0)
+
+
+
+
+for timestep in range(nt):
+    
