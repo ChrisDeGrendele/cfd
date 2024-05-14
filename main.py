947e4d57f091173b8d019fe657f7880126aa0475
ChrisDeGrendele <cjdegrendele@gmail.com>
2024-05-13 21:53:28 -0700

    format with black

--- main.py
+++ main.py
@@ -4,16 +4,13 @@ import numpy as np
 
 nt = 100
 nx = 100
-xlim = (0,1)
+xlim = (0, 1)
 nghost = 2
 numVariables = 1
 ics = np.sin
 
 g = Grid1D(xlim, nx, nghost, numVariables)
-g.fill_grid(ics,0)
+g.fill_grid(ics, 0)
 
 
-
-
-for timestep in range(nt):
-    
+# for timestep in range(nt):
